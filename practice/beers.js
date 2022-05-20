257560238c27f8193c1e69f57dbae53fec633569
Camila Belen <camilabelencapua@gmail.com>
2022-05-19 22:12:27 -0300

    Practica

--- practice/beers.js
+++ practice/beers.js
@@ -1,28 +1,63 @@
-/**
- * En el siguiente array de objetos beers
- * 1.- Desarrollar una function (setPrice) que retorne un array que incluya el precio (price) segun el siguiente criterio
- *     a) Si el  grado alcoholico es < a 5.0 el price es 300
- *     b) Si el grado alcoholico es >= 5.0 el price es 350
- *     c) La cerveza 'Purple Iris' esta de oferta y su price es 320
- * 2.- Desarrolle una función que inserte la propiedad file_name (addFileName) a cada uno de los objetos, esta propiedad debe tener solo
- * el nombre del archivo de la propiedad label (upload_xOMnlK-large.png, etc..)
- * 3.- Desarrollo una funcion que ordene por price (orderPrice)
- * 
- 
- /*
-  Beers
-*/
 const beers = [
-    { name: 'Purple Iris', abv: 6.8, label: 'https://s3.amazonaws.com/brewerydbapi/beer/dMLwGo/upload_yiUllE-large.png', type: 'IPA' },
-    { name: 'Orange Blossom Pilsner', abv: 5.5, label: 'https://s3.amazonaws.com/brewerydbapi/beer/Rczcb9/upload_9Nhxxl-large.png', type: 'Pilsner' },
-    { name: 'Darkness', abv: 4.2, label: 'https://s3.amazonaws.com/brewerydbapi/beer/lnxbIV/upload_idNXFf-large.png', type: 'Stout' },
-    { name: 'Belgian Wit', abv: 5.4, label: 'https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png', type: 'Wheat' },
-    { name: 'Stolen Fruit', abv: 4.6, label: 'https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png', type: 'Wheat' },
-  ];
+  { name: 'Purple Iris', abv: 6.8, label: 'https://s3.amazonaws.com/brewerydbapi/beer/dMLwGo/upload_yiUllE-large.png', type: 'IPA' },
+  { name: 'Orange Blossom Pilsner', abv: 5.5, label: 'https://s3.amazonaws.com/brewerydbapi/beer/Rczcb9/upload_9Nhxxl-large.png', type: 'Pilsner' },
+  { name: 'Darkness', abv: 4.2, label: 'https://s3.amazonaws.com/brewerydbapi/beer/lnxbIV/upload_idNXFf-large.png', type: 'Stout' },
+  { name: 'Belgian Wit', abv: 5.4, label: 'https://s3.amazonaws.com/brewerydbapi/beer/3CvVQG/upload_xOMnlK-large.png', type: 'Wheat' },
+  { name: 'Stolen Fruit', abv: 4.6, label: 'https://s3.amazonaws.com/brewerydbapi/beer/YGT30k/upload_uVCHP7-large.png', type: 'Wheat' },
+];
 
 function orderBeers(beers){
-  return beers
-    .sort((a,b)=> a.type > b.type ? 1:-1);
+return beers
+  .sort((a,b)=> a.type > b.type ? 1:-1);
 }
 
-console.log(orderBeers(beers));
+/* 1.- Desarrollar una function (setPrice) que retorne un array que incluya el precio (price) segun el siguiente criterio 
+a) Si el  grado alcoholico es < a 5.0 el price es 300
+*     b) Si el grado alcoholico es >= 5.0 el price es 350
+*     c) La cerveza 'Purple Iris' esta de oferta y su price es 320 */
+
+const gradoAlcolico = 5.0
+const priceMenor = 300
+const priceMayor = 350
+const pricePurple = 320
+
+function setPrice(){
+return beers.map(beer => {
+  if(beer.name != 'Purple Iris'){
+    if(beer.abv<gradoAlcolico){
+      beer.price = priceMenor
+    }else{
+      beer.price = priceMayor
+    }  
+  }else{
+    beer.price = pricePurple
+  }
+  return beer
+})
+}
+
+console.log("-------------------------------------")
+console.log("con los precios: ")
+console.log(setPrice())
+
+/*Desarrolle una función que inserte la propiedad file_name (addFileName) a cada uno de los objetos, esta propiedad debe tener solo
+el nombre del archivo de la propiedad label (upload_xOMnlK-large.png, etc..) */
+
+function setFileName(){
+return beers.map(beer => {
+  beer.file_name = beer.label
+  return beer
+})
+}
+console.log("-------------------------------------")
+console.log("con el file name: ")
+console.log(setFileName())
+
+/* 3.- Desarrollo una funcion que ordene por price (orderPrice)*/
+function orderPrice(){
+return setFileName().sort((a,b)=> a.price > b.price ? 1:-1);
+}
+
+console.log("-------------------------------------")
+console.log("Ordenado por precio: ")
+console.log(orderPrice())
--- practice/crud-api-inventors.js
+++ practice/crud-api-inventors.js
@@ -1,9 +1,93 @@
+import express from 'express';
+import chalk from 'chalk';
+import fs from 'fs';
 
-//TODO: Tomando el archivo json inventors
+
+const PORT = 3100;
+const app = express();
+
+app.use(express.json());
+//app.use(express.urlencoded({ extended: false }));
+//Tomando el archivo json inventors
+//Lo leo porque es un JSON, no un JS.
 
 // GET /api/inventors --> retorna todos los inventores
+app.get('/api/inventors', (req, res) => {
+    try{
+        const inventors = fs.readFileSync('./inventors.json', 'utf-8');
+        res.json(JSON.parse(inventors));
+    }catch(err){
+        res.sendStatus(400).json(err)
+    }
+    
+})
+
 // GET /api/inventors/:id --> retorna un inventor determinado por ID
+app.get('/api/inventors/:id', (req, res) => {
+    try{
+        const id = parseInt(req.params.id);
+        const inventors = fs.readFileSync('./inventors.json', 'utf-8');
+        const inventorsJson = JSON.parse(inventors);
+        const inventor = inventorsJson.filter(inventor => inventor._id === id)
+        res.json(inventor);
+    }catch(err){
+        res.sendStatus(400).json(err)
+    }
+    
+})
+
 // POST /api/inventors/  --> da de alta un nuevo inventor
+app.post('/api/inventors', (req, res) => {
+    try{
+        const inventorNuevo = req.body;
+        const inventors = fs.readFileSync('./inventors.json', 'utf-8');
+        const inventores = JSON.parse(inventors);
+        inventores.push(inventorNuevo);
+        fs.writeFileSync('./inventors.json', JSON.stringify(inventores, null, ' '));
+        res.sendStatus(200); 
+    }catch(err){
+        res.sendStatus(400).json(err)
+    }
+})
+
 // PUT /api/inventors/:id  --> actualiza las propiedades de un inventor segund ID
+app.put('/api/inventors/:id', (req, res) => {
+    try{
+        const id = parseInt(req.params.id);
+        const inventors = fs.readFileSync('./inventors.json', 'utf-8');
+        const inventores = JSON.parse(inventors);
+        const newInventor = req.body
+        inventores.forEach(element => {
+            if(element._id === id){
+                element._id = newInventor._id
+                element.first = newInventor.first
+                element.last = newInventor.last
+                element.year = newInventor.year
+            }
+        });
+        fs.writeFileSync('./inventors.json', JSON.stringify(inventores, null, ' '));
+        res.json(inventores)
+    }catch(err){
+        res.sendStatus(400).json(err)
+    }
+})
+
 // DELETE /api/inventors/:id  --> elimina un inventor por ID
+app.delete('/api/inventors/:id', (req, res) => {
+    try{
+        const id = parseInt(req.params.id);
+        const inventors = fs.readFileSync('./inventors.json', 'utf-8');
+        const inventores = JSON.parse(inventors);
+        const inventoresFiltrados = inventores.filter(inventor => inventor._id !== id);
+        fs.writeFileSync('./inventors.json', JSON.stringify(inventoresFiltrados, null, ' '));
+        res.sendStatus(200);
+    }catch(err){
+        res.sendStatus(400).json(err)
+    }
+})
+
+
+app.listen(PORT, () => {
+    console.log(chalk.bgGreen.blue(`Server express levantado en el puerto: ${PORT}`));
+});
 
